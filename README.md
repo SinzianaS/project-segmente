32b18317484b7ec563369ea1054138676cd6a48d
SinzianaS <succubys@yahoo.com>
2017-05-28 23:38:25 +0300

    Adding project

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a repository for the dev.network4 Segmente NGO project.
---
+++ main.js
@@ -0,0 +1,17 @@
+// Function for logo and text animation on window load in frontpage.html
+
+window.onload= function(){
+    
+    let text1 = document.getElementById("text1")
+    let text2 = document.getElementById("text2")
+    let image = document.getElementById("logointro")
+    image.style.height= "40%"
+    image.style.width="20%"
+    image.style.opacity=1
+    text1.style.opacity= 1
+    text2.style.opacity= 1
+    text1.style.fontSize= "24px"
+    text2.style.fontSize= "24px"
+    
+}
+
